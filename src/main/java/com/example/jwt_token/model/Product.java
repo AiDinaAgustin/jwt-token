3aff221db4d6d94276d6c31c761cec35c48e1f55
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-10-01 09:31:23 +0700

    inital project

---
+++ src/main/java/com/example/jwt_token/model/Product.java
@@ -0,0 +1,22 @@
+package com.example.jwt_token.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+}
